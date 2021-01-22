b326f15cf48c29ef2185e27cd60c5fe42b1cd59b
jeongk469 <jeongk469@gmail.com>
2021-01-21 22:17:00 -0800

    first problem

--- Day2/src/ss_day2_assign/Addnumber.java
+++ Day2/src/ss_day2_assign/Addnumber.java
@@ -1,4 +1,69 @@
 package ss_day2_assign;
 
+
+/**
+ * Jeong Kim
+ *
+ * Class to take argument from commandline and print out sum.
+ */
+
 public class Addnumber {
+
+    /**
+     *
+     * @param String argument from command line
+     */
+
+    public static void main(String[] args) {
+
+        int total = 0;
+
+        try {
+
+            //check for null
+            nullCheck(args);
+
+            for(String arg: args){
+
+                //Conver string to int but if not goes to catch block
+                int number = Integer.parseInt(arg);
+
+                total = total + number;
+
+            }
+
+            System.out.println("Total sum is " + total);
+
+
+        }
+
+        catch(NullPointerException e){
+
+            System.out.println("Number is required please input numbers");
+
+        }
+
+        catch(NumberFormatException e) {
+
+            System.out.println("You must input a number not characters");
+
+        }
+
+
+    }
+
+    /**
+     *
+     * @param String array from commandline
+     * @throws NullPointerException
+     */
+
+    public static void nullCheck(String[] args) throws NullPointerException{
+
+        if(args.length == 0){
+            throw new NullPointerException();
+        }
+
+    }
+
 }
